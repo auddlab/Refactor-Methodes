a83a58431c1b9e3cd1b460e2e9630fa0d0fa132e
auddlab <audreylabb@hotmail.com>
2026-09-03 17:48:45 -0400

    demo
    mis lol en commentaire

--- src/main/app/Main.java
+++ src/main/app/Main.java
@@ -20,5 +20,6 @@ public class Main {
         ShipmentService service = new ShipmentService(new PricingService(), new PermissionService(),
                 new ManifestRepository(), new NotificationService());
         System.out.println(service.validateCalculatePrintSaveAndNotify(shipment));
+        //lollll
     }
 }
